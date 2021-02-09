541cff5595ddd38e54b033a4a2f00c5fe6bfadbf
CharlotteCarbaugh <77706059+CharlotteCarbaugh@users.noreply.github.com>
2021-02-09 17:47:25 -0600

    Add files via upload

---
+++ Homework 1/main_1.12.py
@@ -0,0 +1,10 @@
+#Charlotte Carbaugh, Student ID: 1815532
+
+user_num = int(input("Enter integer:\n"))
+print("You entered:", user_num)
+print(user_num, "squared is", user_num*user_num)
+print("And", user_num, "cubed is", user_num**3, "!!")
+
+user_num1 = int(input("Enter another integer:\n"))
+print(user_num, "+", user_num1, "is", user_num+user_num1)
+print(user_num, "*", user_num1, "is", user_num*user_num1)
---
+++ Homework 1/main_1.2.py
@@ -0,0 +1,6 @@
+#Charlotte Carbaugh, Student ID 1815532
+#Homework 1,1.12 CIS 2348 zyLab training: Basics
+
+userNum = int(input())
+userNumSquared = userNum * userNum
+print(userNumSquared, end="\n")
