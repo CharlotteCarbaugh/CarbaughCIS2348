0a1aeae2aedde7e7fdbab7b0e3e898601bc1c85d
CharlotteCarbaugh <77706059+CharlotteCarbaugh@users.noreply.github.com>
2021-02-09 16:35:33 -0600

    Add files via upload

---
+++ Homework 1/main.py
@@ -0,0 +1,6 @@
+#Charlotte Carbaugh, 2021SP-18349-CIS2348-Information Systems Application Development
+#Homework 1,1.12 CIS 2348 zyLab training: Basics
+
+userNum = int(input())
+userNumSquared = userNum * userNum
+print(userNumSquared, end="\n")
