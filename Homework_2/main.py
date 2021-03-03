a1accab7605c66172327187986255e28c54717fa
CharlotteCarbaugh <77706059+CharlotteCarbaugh@users.noreply.github.com>
2021-03-02 22:16:00 -0600

    Add files via upload

---
+++ Homework_2/main.py
@@ -0,0 +1,14 @@
+#Carbaugh, Student ID 1815532
+test_str = input().lower()
+new_str = test_str.replace(" ", "")
+y = 0
+
+while y < len(new_str):
+    if new_str[y] == new_str[-1-y]:
+        y += 1
+    else:
+        print(new_str, "is not a palindrome")
+        break
+
+if y == len(new_str):
+    print(test_str,"is a palindrome")
